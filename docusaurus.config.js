79d6f60f629bf41a9a0dfcb08d9ae7f47e5dce69
kazvee <kazveedev@gmail.com>
2025-11-18 19:12:10 -0700

    Migrate onBrokenMarkdownLinks config for v4

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,11 @@ const config = {
   projectName: 'provisioning', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+    markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn', // v4
+    },
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
